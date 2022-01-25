ccf3f1f5918930e0750f2ddbf76078d0f69609ee
JoDsnr <jodesnoyer@gmail.com>
2022-01-25 13:30:24 +0100

    communaute

--- Communaute.cpp
+++ Communaute.cpp
@@ -24,7 +24,7 @@ void Communaute::arreterSur(Joueur* player){
     cout << player->getNom() << " a tire la carte :  " <<  endl;
     sleep_for(300ms);
     srand((unsigned int)time(0));
-    int i = rand() % 10;
+    int i = 10;
     ifstream liste_carte_communaute("Carte_Caisse_Communaute/Communaute" + to_string(i) + ".txt");
     if(liste_carte_communaute){
         string descr;
@@ -32,6 +32,7 @@ void Communaute::arreterSur(Joueur* player){
         cout << descr << endl;
         string methode;
         getline(liste_carte_communaute, methode);
+        cout << methode << endl;
         string info;
         getline(liste_carte_communaute, info);
 
@@ -81,6 +82,82 @@ void Communaute::arreterSur(Joueur* player){
                 (*player->getPion())++;}
             cout <<   player->getNom() << " a donc ete emmene.e en prison, il/elle ne passe pas par la case depart et ne recoit pas 20 000 francs"<< endl;
             }
+
+        else if(methode == "h"){
+            int somme_du;
+            somme_du = stoi(info);
+            cout << "Que choississez vous ?" << endl;
+            cout << "Si vous souhaitez payer l'amende tapez 1 sinon taper 2" << endl;
+            int reponse;
+            cin >> reponse;
+            if(reponse == 1){
+                cout << player->getNom() << " perd F. " << somme_du << endl;
+                (*player)-=somme_du;}
+
+            else{
+                cout << player->getNom() << " a tire la carte :  " <<  endl;
+                sleep_for(300ms);
+                srand((unsigned int)time(0));
+                int i = rand() % 10;
+                ifstream liste_carte_chance("Carte_Chance/Chance" + to_string(i) + ".txt");
+                if(liste_carte_chance){
+                    string descr;
+                    getline(liste_carte_chance, descr);
+                    cout << descr << endl;
+                    string methode;
+                    getline(liste_carte_chance, methode);
+                    string info;
+                    getline(liste_carte_chance, info);
+
+                    if(methode == "p"){
+                        int somme_due;
+                        somme_due = stoi(info);
+                        int paiement = min(player->getSolde(), somme_due);
+                        cout << player->getNom() << " perd F. " << paiement << endl;
+                        (*player)-=paiement;
+
+                        Case * c = this;
+                        while(c->getName() != "parck_gratuit"){
+                            c = c->getSuivante();}
+
+                        ParckGratuit* p = (ParckGratuit*)c;
+                        p->addcagnotte(paiement);
+                        }
+                        
+                    else if(methode == "g"){
+                        int somme_percue;
+                        somme_percue = stoi(info);
+                        cout << player->getNom() << " gagne F. " << somme_percue << endl;            
+                        (*player)+=somme_percue;}
+
+                    else if(methode == "d"){
+                        while(player->getPion()->getPtCase()->getName() != info){
+                            (*player->getPion())++;
+                            if(player->getPion()->getPtCase()->getName() == "Case Depart"){
+                                (*player)+=20000;
+                                cout << player->getNom() << " est passe par la case depart et gagne donc F 20.000 " << endl;   
+                            }
+                        }
+                        cout << player->getNom() << " est desormais sur la case " << info << endl;}
+
+                    else if(methode == "r"){
+                        int deplacement(39-stoi(info));
+                        for(int i = 0; i < deplacement; i++){
+                            (*player->getPion())++;}
+                        cout << player->getNom() << " est desormais sur la case " << player->getPion()->getPtCase()->getName() << endl;
+                        player->getPion()->getPtCase()->arreterSur(player);}
+
+                    else if(methode == "x"){
+                        player->setinprison(true);
+                        player->settourinprison(0);
+                        player->setNbDouble(0);
+                        while(player->getPion()->getPtCase()->getName() != info){
+                            (*player->getPion())++;}
+                        cout <<   player->getNom() << " a donc ete emmene.e en prison, il/elle ne passe pas par la case depart et ne recoit pas 20 000 francs"<< endl;
+                        }
+                }
+            }        
+        }
     }
 
 }
